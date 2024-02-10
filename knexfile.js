99215c2398630888f203ae880970cf1c25aa5aab
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-02-10 13:17:51 +0530

    add data

--- knexfile.js
+++ knexfile.js
@@ -2,18 +2,14 @@
 require('dotenv').config();
 
 module.exports = {
-  client: 'mssql',
+  client: 'mysql2',
   connection: {
-    server: 'DESKTOP-SKTGI2Q',
-    user: 'mukeshkanna',
-    password: 'leo45gkm',
-    database: 'kodukku_test_project',
+    host: 'sql.freedb.tech',
+    user: 'freedb_liveroot',
+    password: 'dn%&s6fN8*V#K4m',
+    database: 'freedb_kodukku_test_project',
     charset: 'utf8',
-   
-    options: {
-      encrypt: false, 
-      
-    },
+    port: 3306
   },
   migrations: {
     tableName: 'knex_migrations',
@@ -23,23 +19,3 @@ module.exports = {
     directory: './seeds',
   },
 };
-
-
-// module.exports = {
-//   client: 'mysql',
-//   connection: {
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'kodukku_test_project',
-//     charset: 'utf8',
-//   },
-  
-//   migrations: {
-//     tableName: 'knex_migrations',
-//     directory: './migrations',
-//   },
-//   seeds: {
-//     directory: './seeds',
-//   },
-// };
